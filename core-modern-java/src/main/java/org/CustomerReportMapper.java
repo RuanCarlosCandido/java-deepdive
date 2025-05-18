de585704781f1d307dea8f4b4a89420cd5f4bd0c
RuanCarlosCandido <ruan.candido@hotmail.com>
2025-05-18 18:33:21 -0300

    first commit

--- core-modern-java/src/main/java/org/CustomerReportMapper.java
+++ core-modern-java/src/main/java/org/CustomerReportMapper.java
@@ -3,83 +3,48 @@ package org;
 import java.math.BigDecimal;
 import java.time.YearMonth;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CustomerReportMapper {
 
-        public static CustomerReport from(Transaction transaction, CurrencyConverter currencyConverter) {
-            if (!currencyConverter.currencyMap().containsKey(transaction.currency()))
-                throw new IllegalArgumentException("Currency not mapped");
-    
-            BigDecimal amountInBRL = transaction.value()
-                    .multiply(currencyConverter.currencyMap().get(transaction.currency()));
-    
-            Map<TransactionType, Integer> countsByType = new HashMap<>();
-            countsByType.put(transaction.transactionType(), 1);
-    
-            Map<YearMonth, BigDecimal> monthValue = new HashMap<>();
-            monthValue.put(transaction.yearMonth(), amountInBRL);
-    
-            return new CustomerReport(
-                    amountInBRL,
-                    countsByType,
-                    transaction.yearMonth(),
-                    monthValue
-            );
+        public static CustomerReport from(Transaction tx, CurrencyConverter cc) {
+                BigDecimal amountInBRL = tx.value().multiply(cc.currencyMap().get(tx.currency()));
+
+                return new CustomerReport(
+                                amountInBRL,
+                                Map.of(tx.transactionType(), 1),
+                                tx.yearMonth(),
+                                Map.of(tx.yearMonth(), amountInBRL));
         }
-    
-        public static CustomerReport from(Transaction transaction, CurrencyConverter currencyConverter,
-                                          Map<String, CustomerReport> userReports) {
-    
-            String userId = transaction.user();
-            BigDecimal amountInBRL = transaction.value()
-                    .multiply(currencyConverter.currencyMap().get(transaction.currency()));
-    
-            CustomerReport existingReport = userReports.get(userId);
-    
-            // Defensive copies
-            Map<TransactionType, Integer> countsByType = new HashMap<>(existingReport.transactionCountByType());
-            Map<YearMonth, BigDecimal> monthValue = new HashMap<>(existingReport.monthValue());
-    
-            // Atualiza contagem por tipo
-            countsByType.merge(transaction.transactionType(), 1, Integer::sum);
-    
-            // Atualiza valor mensal se for maior
-            monthValue.merge(transaction.yearMonth(), amountInBRL, (oldVal, newVal) ->
-                    newVal.compareTo(oldVal) > 0 ? newVal : oldVal
-            );
-    
-            // Define novo peakMonth
-            YearMonth peakMonth = monthValue.entrySet().stream()
-                    .max(Comparator.comparing(Entry::getValue))
-                    .map(Entry::getKey)
-                    .orElse(transaction.yearMonth());
-    
-            BigDecimal total = existingReport.totalInBRL().add(amountInBRL);
-    
-            return new CustomerReport(total, countsByType, peakMonth, monthValue);
-        }
-    
+
         public static CustomerReport merge(CustomerReport r1, CustomerReport r2) {
-            BigDecimal total = r1.totalInBRL().add(r2.totalInBRL());
-    
-            Map<TransactionType, Integer> combinedCounts = new HashMap<>(r1.transactionCountByType());
-            r2.transactionCountByType().forEach((type, count) ->
-                    combinedCounts.merge(type, count, Integer::sum)
-            );
-    
-            Map<YearMonth, BigDecimal> combinedMonthValues = new HashMap<>(r1.monthValue());
-            r2.monthValue().forEach((month, value) ->
-                    combinedMonthValues.merge(month, value, (v1, v2) -> v1.max(v2))
-            );
-    
-            YearMonth peakMonth = combinedMonthValues.entrySet().stream()
-                    .max(Comparator.comparing(Entry::getValue))
-                    .map(Entry::getKey)
-                    .orElse(null);
-    
-            return new CustomerReport(total, combinedCounts, peakMonth, combinedMonthValues);
+                BigDecimal total = r1.totalInBRL().add(r2.totalInBRL());
+
+                // Soma as contagens por tipo de transação
+                Map<TransactionType, Integer> countsByType = Stream
+                                .concat(r1.transactionCountByType().entrySet().stream(),
+                                                r2.transactionCountByType().entrySet().stream())
+                                .collect(Collectors.toMap(
+                                                Map.Entry::getKey,
+                                                Map.Entry::getValue,
+                                                Integer::sum));
+
+                // Combina os valores mensais, mantendo o maior valor de cada mês
+                Map<YearMonth, BigDecimal> monthValues = Stream
+                                .concat(r1.monthValue().entrySet().stream(), r2.monthValue().entrySet().stream())
+                                .collect(Collectors.toMap(
+                                                Map.Entry::getKey,
+                                                Map.Entry::getValue,
+                                                BigDecimal::max));
+
+                // Determina o mês de maior valor
+                YearMonth peakMonth = monthValues.entrySet().stream()
+                                .max(Comparator.comparing(Map.Entry::getValue))
+                                .map(Map.Entry::getKey)
+                                .orElse(null);
+
+                return new CustomerReport(total, countsByType, peakMonth, monthValues);
         }
-    }
+}
--- core-modern-java/src/main/java/org/TransactionAnalyzer.java
+++ core-modern-java/src/main/java/org/TransactionAnalyzer.java
@@ -1,22 +1,11 @@
 package org;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TransactionAnalyzer {
 
     public static Map<String, CustomerReport> analyze(Stream<Transaction> transactionsStream,
-            CurrencyConverter currencyConverter) {
-
-        return solucaoFuncional(transactionsStream, currencyConverter);
-
-        // return solucaoImperativa(transactionsStream, currencyConverter);
-    }
-
-    private static Map<String, CustomerReport> solucaoFuncional(Stream<Transaction> transactionsStream,
             CurrencyConverter currencyConverter) {
 
         return transactionsStream
@@ -26,45 +15,6 @@ public class TransactionAnalyzer {
                                 Transaction::user,
                                 tx -> CustomerReportMapper.from(tx, currencyConverter),
                                 CustomerReportMapper::merge));
-
-    }
-
-    private static Map<String, CustomerReport> solucaoImperativa(Stream<Transaction> transactionsStream,
-            CurrencyConverter currencyConverter) {
-        List<Transaction> transactions = transactionsStream.collect(Collectors.toList());
-
-        Map<String, CustomerReport> userReports = new HashMap<>();
-
-        for (Transaction transaction : transactions) {
-
-            userReports.put(transaction.user(), calculate(currencyConverter, userReports, transaction));
-
-        }
-
-        System.out.println(userReports);
-        return userReports;
-    }
-
-    private static CustomerReport calculate(CurrencyConverter currencyConverter,
-            Map<String, CustomerReport> userReports, Transaction transaction) {
-
-        String userId = transaction.user();
-
-        CustomerReport finalReport;
-        if (!currencyConverter.currencyMap().containsKey(transaction.currency()))
-            throw new IllegalArgumentException("Currency not mapped");
-
-        // Tentar extrair a logica daqui pra classe CustomerReportMapper ou outra coisa
-        // semelhante, facilitando o uso dos streams
-        if (userReports.containsKey(userId)) {
-
-            finalReport = CustomerReportMapper.from(transaction, currencyConverter, userReports);
-
-        } else {
-
-            finalReport = CustomerReportMapper.from(transaction, currencyConverter);
-        }
-        return finalReport;
     }
 
 }
